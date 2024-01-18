f9d3b70f5fa258754f851d28d8f5ce07c72af5e3
FrankTarco <jhoanangelo123.tc@gmail.com>
2024-01-18 18:25:44 -0500

    Front Client

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddDashboardComponent } from './add-dashboard/add-dashboard.component';
+import { MainComponent } from './client/main/main.component';
 
 const routes: Routes = [
-  {path:'app',component:AddDashboardComponent}
+  {path:'app',component:AddDashboardComponent},
+  {path:'',component:MainComponent}
 ];
 
 @NgModule({
